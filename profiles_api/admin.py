9149b9355a2bc9c3468d6bd7c35f94320d4b315b
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-04 08:17:49 -0300

    user model and set admin

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .import models
 
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name' , 'is_active', 'is_staff']
+
+admin.site.register(models.UserProfile , UserProfileAdmin)
