631bc0da6f8220d773ce6e4c042e24f6d5ade665
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-04 10:05:10 -0300

    mthod

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,12 @@
+from curses import noecho
+from email import message
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .import serializers
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         an_apiview = [
             'Usamos method http',
@@ -13,3 +18,35 @@ class HelloApiView(APIView):
             'an_apiview' : an_apiview
         }
         return Response(context)
+
+    def post(self, request):
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello' +' '+ name
+
+            context= {
+                'message': message
+            }
+
+            return Response(context)
+        else:
+            context = {
+                'error' : serializers.error,
+                'status' : status.HTTP_400_BAD_REQUEST
+            }
+            return Response(context)
+
+
+    def put(self, request, pk=None):
+        return Response({'method' : 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method' : 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method' : 'delete'})
+
+
+
